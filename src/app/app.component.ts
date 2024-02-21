1001e6567333a15b08d717673e35848cc13800cb
Aspergillusplay <kartofka123@i.ua>
2024-02-21 18:44:29 +0200

    Fix bugs with checkboxes and cleaned code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgForOf } from '@angular/common';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -24,35 +23,10 @@ export class AppComponent{
     }
     return binaryArray;
   }
-
-  // private fromBinaryToDecimal(box: number[]): number {
-  //   console.log()
-  //   // 1010
-  //   // 1*8+0*4+1*2+0*1
-  //   let result = 0;
-  //   let sqrt = 1;
-  //   const binaryLenght = box.length;
-  //
-  //   // for(let i= 0; i<binaryLenght; i++) {
-  //   //   sqrt = Math.pow(2, (binaryLenght - i));
-  //   //   result += box[i] * sqrt;
-  //   //   console.log(sqrt)
-  //   // }
-  //
-  //   for(let i= binaryLenght; i >= 0; i--) {
-  //     result += box[i] * sqrt;
-  //     sqrt *= 2;
-  //     console.log(result)
-  //   }
-  //
-  //   return result;
-  // }
-
   private fromBinaryToDecimal(binary: number[]): number {
     let result = 0;
     for(let i = binary.length - 1; i >= 0; i--) {
       result += binary[i] * Math.pow(2, binary.length - 1 - i);
-      console.log(Math.pow(2, binary.length - 1 - i))
     }
     return result;
   }
