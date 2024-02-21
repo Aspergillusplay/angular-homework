96b44781fd0b915240a26fa381fe3dc90b3f148e
DmitryByrigin <dmitri.d907@gmail.com>
2024-02-21 17:05:03 +0100

    Second part of homework "Converter"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgForOf } from '@angular/common';
@@ -25,10 +25,45 @@ export class AppComponent{
     return binaryArray;
   }
 
+  // private fromBinaryToDecimal(box: number[]): number {
+  //   console.log()
+  //   // 1010
+  //   // 1*8+0*4+1*2+0*1
+  //   let result = 0;
+  //   let sqrt = 1;
+  //   const binaryLenght = box.length;
+  //
+  //   // for(let i= 0; i<binaryLenght; i++) {
+  //   //   sqrt = Math.pow(2, (binaryLenght - i));
+  //   //   result += box[i] * sqrt;
+  //   //   console.log(sqrt)
+  //   // }
+  //
+  //   for(let i= binaryLenght; i >= 0; i--) {
+  //     result += box[i] * sqrt;
+  //     sqrt *= 2;
+  //     console.log(result)
+  //   }
+  //
+  //   return result;
+  // }
+
+  private fromBinaryToDecimal(binary: number[]): number {
+    let result = 0;
+    for(let i = binary.length - 1; i >= 0; i--) {
+      result += binary[i] * Math.pow(2, binary.length - 1 - i);
+      console.log(Math.pow(2, binary.length - 1 - i))
+    }
+    return result;
+  }
   onChangeInput(){
     if(this.hodnota.value){
       this.boxes = this.fromDecimalToBinary(this.hodnota.value);
     }
+  }
+  onChangeCheckbox(index: number){
+    this.boxes[index] = Number(!this.boxes[index]);
+    this.hodnota.setValue(this.fromBinaryToDecimal(this.boxes));
   }
 }
 
