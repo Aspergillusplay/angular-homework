d5215c41f4af419c615c2af6cca27b360784f82a
DmitryByrigin <dmitri.d907@gmail.com>
2024-03-06 22:02:42 +0100

    Implement Book Component

--- src/book/book.component.ts
+++ src/book/book.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+
+interface Kniha {
+  nazov: string | null;
+  autor: string | null;
+  rokVydania: number | null;
+}
 
 @Component({
   selector: 'app-book',
-  standalone: true,
-  imports: [],
   templateUrl: './book.component.html',
-  styleUrl: './book.component.css'
+  standalone: true,
+  imports: [
+    NgForOf,
+    ReactiveFormsModule,
+    NgOptimizedImage
+  ],
+  styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+export class BookComponent{
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  rok: number = new Date().getFullYear();
+
+
+  formular = this.formBuilder.group({
+    nazov: ['', Validators.required],
+    autor: ['', Validators.required],
+    rokVydania: [this.rok, [Validators.required, Validators.min(0)]]
+  })
+
+  knihy: Kniha[] = [];
+
+  onSubmit() {
+    this.knihy.push(this.formular.value as Kniha);
+    console.log(this.knihy);
+    this.formular.reset();
+  }
+
+  nastavForm(){
+    this.formular.setValue({
+      nazov: 'Vojna a mier',
+      autor: 'Lev Tolstoj',
+      rokVydania: 1869
+    });
+  }
 }
