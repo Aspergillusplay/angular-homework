c013bafa188553af8af77d9015fca241364eb568
Aspergillusplay <kartofka123@i.ua>
2024-03-13 18:35:18 +0200

    Made a connection between the frontend and the backend for the user

--- src/user/user.component.ts
+++ src/user/user.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgForOf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment.prod";
 
 interface Zakaznik {
-  meno: string | null;
-  priezvisko: string | null;
-  adresa: {
-    ulica: string | null;
-    mesto: string | null;
-  };
+  firstName: string | null;
+  lastName: string | null;
+  email: string | null;
+  // adresa: {
+  //   ulica: string | null;
+  //   mesto: string | null;
+  // };
 }
 @Component({
   selector: 'app-user',
@@ -22,35 +25,50 @@ interface Zakaznik {
 })
 export class UserComponent {
 
-  constructor(private formBilder: FormBuilder) {
+  constructor(private formBilder: FormBuilder, private http: HttpClient) {
   }
   hodnota = new FormControl('');
 
   formular = this.formBilder.group({
-    meno: ['', Validators.required],
-    priezvisko: ['', Validators.required],
-    adresa: this.formBilder.group({
-      ulica: ['', Validators.required],
-      mesto: ['', Validators.required]
-    })
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required]
+    // adresa: this.formBilder.group({
+    //   ulica: ['', Validators.required],
+    //   mesto: ['', Validators.required]
+    // })
   })
+  ngOnInit() {
+    this.http.get<Zakaznik[]>(`${environment.BACKEND_URL}/api/customers`).subscribe(zakaznici => {
+      this.zakaznici = zakaznici;
+    });
+  }
 
   zakaznici: Zakaznik[] = []; // массив для хранения данных клиентов
 
   onSubmit() {
-    this.zakaznici.push(this.formular.value as Zakaznik);
-    console.log(this.zakaznici);
+    const formValue = this.formular.value;
+    const newZakaznik: Zakaznik = {
+      firstName: formValue.firstName || '',
+      lastName: formValue.lastName || '',
+      email: formValue.email || ''
+    };
+    this.http.post(`${environment.BACKEND_URL}/api/customers`, newZakaznik).subscribe(response => {
+      console.log(response);
+      this.ngOnInit();
+    });
     this.formular.reset();
   }
 
   nastavForm(){
     this.formular.setValue({
-      meno: 'Artem',
-      priezvisko: 'Ponomarov',
-      adresa: {
-        ulica: "Tajovskeho 30",
-        mesto: "Banska Bystrica",
-      }
+      firstName: 'Artem',
+      lastName: 'Ponomarov',
+      email: 'kar@u.com'
+      // adresa: {
+      //   ulica: "Tajovskeho 30",
+      //   mesto: "Banska Bystrica",
+      // }
     });
   }
 }
